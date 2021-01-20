d979b50423f02c8ac45f2ccbe0069f469feb7d2f
sasakipages <sasaki.kenta@outlook.com>
2021-01-21 04:12:28 +0900

    Save TAsk

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './component/Header'
 import Tasks from './component/Tasks'
+import AddTask from './component/AddTask'
 import { useState } from 'react'
 
 const App =()=> {
@@ -23,6 +24,12 @@ const App =()=> {
       reminder: false,
     },
   ])
+
+  const addTask =(task)=> {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
  
   const deleteTask =(id)=> {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -35,6 +42,7 @@ const App =()=> {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
   );
